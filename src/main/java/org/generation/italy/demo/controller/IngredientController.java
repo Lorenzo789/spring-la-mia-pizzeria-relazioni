788f1a79522322ee80a06b0486288a43e55fe785
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-12-14 10:47:31 +0100

    edit ingredient

--- src/main/java/org/generation/italy/demo/controller/IngredientController.java
+++ src/main/java/org/generation/italy/demo/controller/IngredientController.java
@@ -1,6 +1,7 @@
 package org.generation.italy.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.generation.italy.demo.pojo.Ingredient;
 import org.generation.italy.demo.pojo.Pizza;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -68,6 +70,56 @@ public class IngredientController {
 			p.getIngredients().add(ingredient);
 		}
 
+		ingredientService.save(ingredient);
+		
+		return "redirect:/ingredient";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String edit(@PathVariable("id") int id, Model model) {
+		
+		Optional<Ingredient> optIngredient = ingredientService.findById(id);
+		
+		Ingredient ingredient = optIngredient.get();
+		
+		model.addAttribute("ingredient", ingredient);
+		
+		List<Pizza> pizze = pizzaService.findAll();
+		
+		model.addAttribute("pizze", pizze);
+		
+		return "edit-ingredient";
+	}
+	
+	@PostMapping("/update")
+	public String update(@Valid Ingredient ingredient, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if (bindingResult.hasErrors()) {
+			
+			System.err.println("ERROR ------------------------------------------");
+			System.err.println(bindingResult.getAllErrors());
+			System.err.println("------------------------------------------------");
+			
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			
+			return "redirect:/ingredient";
+		}
+		
+		List<Pizza> pizze = ingredient.getPizze();
+		for (Pizza p : pizze) {
+			
+			List<Ingredient> ingredientPresent = p.getIngredients();
+			
+			if (p.getIngredients().equals(ingredientPresent)) {
+				
+				System.err.println(p.getIngredients().equals(ingredientPresent));
+				
+			} else {
+				
+				p.getIngredients().add(ingredient);
+			}
+		}
+		
 		ingredientService.save(ingredient);
 		
 		return "redirect:/ingredient";
